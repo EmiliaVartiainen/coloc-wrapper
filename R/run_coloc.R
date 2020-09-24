9d08ef54f79e65a051123746b0e95f86b4e18541
EmiliaVartiainen <emilia.vartiainen@helsinki.fi>
2020-09-24 14:32:37 +0300

    added gene functionality

--- R/run_coloc.R
+++ R/run_coloc.R
@@ -27,44 +27,54 @@ run_coloc <- function(eqtl_data, gwas_data, out) {
 
     ## select ------------------------
     df_gwas <- df_gwas[,c("rsids", "pval", "maf")]
-    df_eqtl <- df_eqtl[,c("rsid", "pvalue", "maf")]
+    df_eqtl <- df_eqtl[,c("rsid", "pvalue", "maf", "gene_id")]
     
     ## join -------------------------
     df <- dplyr::inner_join(df_gwas, df_eqtl, by = c("rsids" = "rsid"))
     #print(head(df))
     
+    genes <- unique(df$gene_id)
+    
+    
+    my.res <- lapply(genes, function(x) {
+                    df_sub <- df[which(df$gene_id == x),]
+                    
+                    ## create data lists ------------
+                    dataset_gwas <- list(
+                        pvalues = df_sub$pval, 
+                        MAF = df_sub$maf.x,
+                        type = "cc", 
+                        s = 11006/117692, 
+                        #  sdY = 1,
+                        snp = df_sub$rsids,
+                        N = n_gwas)
+                    
+                    dataset_eqtl <- list(
+                        pvalues = df_sub$pvalue, 
+                        MAF = df_sub$maf.y,
+                        type = "quant", 
+                        sdY = 1,
+                        snp = df_sub$rsid,
+                        N = n_eqtl)
+                    
+                    ## run coloc.abf() --------------------------
+                    res <- coloc::coloc.abf(dataset1=dataset_gwas, dataset2=dataset_eqtl, p1 = 1e-4, p2 = 1e-4, p12 = 1e-5)
+                    return (data.frame(gene = x, t(res$summary)))
+    })
 
-    ## create data lists ------------
-    dataset_gwas <- list(
-        pvalues = df$pval, 
-        MAF = df$maf.x,
-        type = "cc", 
-        s = 11006/117692, 
-        #  sdY = 1,
-        snp = df$rsids,
-        N = n_gwas)
-    
-    dataset_eqtl <- list(
-        pvalues = df$pvalue, 
-        MAF = df$maf.y,
-        type = "quant", 
-        sdY = 1,
-        snp = df$rsid,
-        N = n_eqtl)
-    
-    ## run coloc.abf() --------------------------
-    my.res <- coloc::coloc.abf(dataset1=dataset_gwas, dataset2=dataset_eqtl, p1 = 1e-4, p2 = 1e-4, p12 = 1e-5)
-    data.table::fwrite(data.frame(t(my.res$summary)), file = out, sep = "\t")
+    df <- do.call("rbind", my.res)
+    print(df)
+    data.table::fwrite(df, file = out, sep = "\t")
     
     ## run locuscompare -------------
-    pdf(paste0(out, ".pdf"))
-    locuscomparer::locuscompare(
-        in_fn1 = dir_gwas, in_fn2 = dir_eqtl, 
-        title1 = "GWAS", title2 = "eQTL", 
-        marker_col1 = header_gwas["varid"], pval_col1 = header_gwas["pval"], 
-        marker_col2 = header_eqtl["varid"], pval_col2 = header_eqtl["pval"], 
-        genome = "hg38", snp = "rs11121615")
-    dev.off()
+    #pdf(paste0(out, ".pdf"))
+    #locuscomparer::locuscompare(
+    #    in_fn1 = dir_gwas, in_fn2 = dir_eqtl, 
+    #    title1 = "GWAS", title2 = "eQTL", 
+    #    marker_col1 = header_gwas["varid"], pval_col1 = header_gwas["pval"], 
+    #    marker_col2 = header_eqtl["varid"], pval_col2 = header_eqtl["pval"], 
+    #    genome = "hg38", snp = "rs11121615")
+    #dev.off()
 }
 
-run_coloc(eqtl_data = "extdata/Lepik_2017_ge_blood_chr1_ENSG00000130940.all.tsv", gwas_data = "extdata/I9_VARICVE_chr1.tsv.gz", out = "test1.txt")
+run_coloc(eqtl_data = "extdata/Lepik_2017_ge_blood_chr1_ENSG00000142655_ENSG00000130940.all.tsv", gwas_data = "extdata/I9_VARICVE_chr1.tsv.gz", out = "test1.txt")
