34520698daf4dcfaed7b9a6b6e7401a93e60b86b
Sina Rueger <sina.rueeger@gmail.com>
2020-09-24 10:12:36 +0000

    delete example

--- R/subset_data.R
+++ R/subset_data.R
@@ -3,9 +3,8 @@
 #' @param region chromosomal region of interest, "chr:start-end"
 #' @param outfile output file path
 #' @examples
-#' subset_data_file(infile = "/Users/eahvarti/Documents/coloc_analysis/Data/finngen_R5_I9_VARICVE.gz", region = "1:10565520-10965520", outfile = "/Users/eahvarti/Downloads/delete_me.txt")
-#' subset_data_file(infile = "/COLOC/extdata/I9_VARICVE_chr1.tsv.gz", region = "1:10565520-10965520", outfile = "delete_me.txt")
-#' subset_data_file(infile = "ftp://ftp.ebi.ac.uk/pub/databases/spot/eQTL/csv/Alasoo_2018/ge/Alasoo_2018_ge_macrophage_IFNg.all.tsv.gz", region = "1:10565520-10965520", outfile = "delete_me.txt")
+#' subset_data_file(infile = "/COLOC/extdata/I9_VARICVE_chr1.tsv.gz", region = "1:10565520-10965520", outfile = "delete_me_gwas.txt")
+#' subset_data_file(infile = "ftp://ftp.ebi.ac.uk/pub/databases/spot/eQTL/csv/Alasoo_2018/ge/Alasoo_2018_ge_macrophage_IFNg.all.tsv.gz", region = "1:10565520-10965520", outfile = "delete_me_eqtl.txt")
 
 subset_data_file <- function(infile, region, outfile) {
 
