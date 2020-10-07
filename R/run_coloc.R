f20cf120f9ad04247b057186ff157e86ddfa5df3
Sina Rueger <sina.rueeger@gmail.com>
2020-10-07 15:42:47 +0000

    extending header

--- R/run_coloc.R
+++ R/run_coloc.R
@@ -1,8 +1,8 @@
 
 ## todo
-## - with varids, instead of rsids
-## - z/maf vs beta/se
-## - headers
+## - warnings
+## - locuszoomplot
+## - tests for header
 
 #' @param eqtl_data input file path, ftp or local
 #' @param gwas_data chromosomal region of interest, string in format "chr:start-end"
@@ -14,54 +14,85 @@
 #' @param return_file logical, write out file
 #' @param eqtl_info list containing type, N and depending on type sdY or s.
 #' @param gwas_info list containing type, N and depending on type sdY (if type = quant) or s (if type = cc).
-
+#' @param gwas_header vector containing headers of gwas_data, either c(varid, pvalues, MAF) or c(varid, beta, sebeta, maf).
+#' @param eqtl_header vector containing headers of eqtl_data, either c(varid, gene_id, pvalues, MAF) or c(varid, gene_id, beta, sebeta, maf).
 #' @details for input data and parameters see https://chr1swallace.github.io/coloc/
+#' varid can be any variant identifier, but format needs to match between datasets. 
 #' @examples
 #' run_coloc(eqtl_data = "extdata/Lepik_2017_ge_blood_chr1_ENSG00000130940.all.tsv", gwas_data = "extdata/I9_VARICVE_chr1.tsv.gz", return_object = TRUE)
 
 run_coloc <- function(eqtl_data, gwas_data, out, p1 = 1e-4, p2 = 1e-4, p12 = 1e-5, 
-    return_object = FALSE, return_file = TRUE, eqtl_info = list(type = "quant", sdY = 1, N = 491), 
-    gwas_info = list(type = "cc", s = 11006/117692, N  = 11006	+ 117692)
+    return_object = FALSE, return_file = TRUE, eqtl_info = list(type = "quant", sdY = 1, N = NA), 
+    gwas_info = list(type = "cc", s = NA, N = NA), 
+    gwas_header = c(varid = "rsid", pvalues = "pval", MAF = "maf"),
+    eqtl_header = c(varid = "rsid", pvalues = "pval", MAF = "maf", gene_id = "gene_id")
     ) {
-    
 
-    header_gwas <- c("varid" = "rsids", "pval" = "pval", "maf" = "maf", "beta"= "beta", "se"= "sebeta")
-    header_eqtl <- c("varid" = "rsid", "pval" = "pvalue", "maf" = "maf", "beta" = "beta", "gene_id" = "gene_id")
+    ## check if beta/se or pval/maf --------------------
 
-    #header_gwas <- c("varid" = "rsids", "pval" = "pval", "maf" = "maf")
-    #header_eqtl <- c("varid" = "rsid", "pval" = "pvalue", "maf" = "maf")
 
     ## read data -----------------
 
-    df_eqtl <- data.table::fread(file = eqtl_data) #select = unname(header_eqtl))
-    #names(df_eqtl) <- names(header_eqtl)
-    df_gwas <- data.table::fread(file = gwas_data) #select= unname(header_gwas))
-    #names(df_gwas) <- names(header_gwas)
+    df_eqtl <- data.table::fread(file = eqtl_data, select = unname(eqtl_header))
+    names(df_eqtl) <- paste0(names(eqtl_header), ".eqtl")
+    df_gwas <- data.table::fread(file = gwas_data, select= unname(gwas_header))
+    names(df_gwas) <- paste0(names(gwas_header), ".gwas")
 
-    ## select ------------------------
-    df_gwas <- df_gwas[,c("rsids", "pval", "maf")]
-    df_eqtl <- df_eqtl[,c("rsid", "pvalue", "maf", "gene_id")]
-    
     ## join -------------------------
-    df <- dplyr::inner_join(df_gwas, df_eqtl, by = c("rsids" = "rsid"))
-    #print(head(df))
-    
+    df <- dplyr::inner_join(df_gwas, df_eqtl, by = c("varid.gwas" = "varid.eqtl"))
+
+
+
     ## loop over genes -----------------
     genes <- unique(df$gene_id)
     my.res <- lapply(genes, function(x) {
+
                     df_sub <- df[which(df$gene_id == x),]
-                    
-                    ## create data lists ------------
+
+                    ## create gwas data lists ------------
+
                     dataset_gwas <- gwas_info
-                    dataset_gwas$pvalues <- df_sub$pval
-                    dataset_gwas$MAF <- df_sub$maf.x
-                    dataset_gwas$snp <- df_sub$rsids
+                    dataset_gwas$snp <- df_sub$varid.gwas
+
+                    if ("pvalues" %in% names(gwas_header)) {
+                        dataset_gwas$pvalues <- df_sub$pvalues.gwas
+                    }
+
+                    if ("MAF" %in% names(gwas_header)) {
+                        dataset_gwas$MAF <- df_sub$MAF.gwas
+                    }
+
+                    if ("beta" %in% names(gwas_header)) {
+                        dataset_gwas$beta <- df_sub$beta.gwas
+                    }
+
+                    if ("sebeta" %in% names(gwas_header)) {
+                        dataset_gwas$varbeta <- df_sub$sebeta.gwas^2
+                    }
+
+                    ## create eqtl data lists ------------
                     
                     dataset_eqtl <- eqtl_info
-                    dataset_eqtl$pvalues <- df_sub$pvalue
-                    dataset_eqtl$MAF <- df_sub$maf.y
-                    dataset_eqtl$snp <- df_sub$rsid
-                    
+                    dataset_eqtl$snp <- df_sub$varid.gwas
+
+                    if ("pvalues" %in% names(eqtl_header)) {
+                        dataset_eqtl$pvalues <- df_sub$pvalues.eqtl
+                    }
+
+                    if ("MAF" %in% names(eqtl_header)) {
+                        dataset_eqtl$MAF <- df_sub$MAF.eqtl
+                    }
+                      
+                    if ("beta" %in% names(eqtl_header)) {
+                        dataset_eqtl$beta <- df_sub$beta.eqtl
+                    }
+
+                    if ("sebeta" %in% names(eqtl_header)) {
+                        dataset_eqtl$varbeta <- df_sub$sebeta.eqtl^2
+                    }
+
+
+
                     ## run coloc.abf() --------------------------
                     res <- coloc::coloc.abf(dataset1=dataset_gwas, dataset2=dataset_eqtl, 
                                             p1 = p1, p2 = p2, p12 = p12)
