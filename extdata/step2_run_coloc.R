2eb4c3feafb2fff9f55428ed297abfdf8dddc8b4
Sina Rueger <sina.rueeger@gmail.com>
2020-10-30 18:11:52 +0000

    fix locuscompare argument

--- extdata/step2_run_coloc.R
+++ extdata/step2_run_coloc.R
@@ -32,5 +32,5 @@ run_coloc(eqtl_data = opt$eqtl, gwas_data = opt$gwas,
         p1 = opt$p1, p2 = opt$p2, p12 = opt$p12, 
         eqtl_info = info_eqtl, 
         gwas_info = info_gwas, 
-        locuscompare_thresh = locuscompare_thresh
+        locuscompare_thresh = opt$locuscompare_thresh
       )
